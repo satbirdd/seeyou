eba1561e6ea7dce3d4f6e903dfe3610bf386e9ce
rakeLL <satbirdd@gmail.com>
2013-04-24 14:09:16 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seeyou::Application.config.secret_token = '4431fc6fc00dc43c4ab82a954228705116365ce133aeb56964fdd16691acd26a3018badb01f848984d7277e49de62a5349faf2fcca2d56cb799537c38efbc979'
